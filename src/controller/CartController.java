5dd954f92eda4310a18da97eac3a037e885a1f8c
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-07-13 22:55:15 +0200

    feat(Backend): Cart logic saved in session.

--- src/controller/CartController.java
+++ src/controller/CartController.java
@@ -1,7 +1,40 @@
 package controller;
+import com.google.gson.Gson;
 
-import service.CartService;
+import beans.ArticleInCart;
+import beans.Buyer;
+import beans.Cart;
+import service.UsersService;
+import spark.Request;
+import spark.Route;
+import spark.Session;
 
 public class CartController {
-    private CartService cartService=new CartService();
+    private static Gson gson =new Gson();
+    public static Route addArticleInCart =(request,response)->{
+        response.type("application/json");
+        ArticleInCart articleInCart=gson.fromJson(request.body(), ArticleInCart.class);
+        getCart(request).addArticle(articleInCart);
+        return ("OK");
+    };
+
+    public static Route getCart=(request,respons)->{    
+        return gson.toJson(getCart(request));
+    };
+    public static Route getTotal=(request,respons)->{    
+        return gson.toJson(getCart(request).getPrice());
+    };
+    
+    
+    public static Cart getCart(Request request){
+        Session ss = request.session(true);
+		Cart cart = ss.attribute("cart"); 
+		if (cart == null) {
+            UsersService usersService =new UsersService();
+            cart = new Cart((Buyer)usersService.getByUsername(UserController.getLoggedingUser(request)));
+			ss.attribute("cart", cart);
+		}
+		return cart;
+    }
+
 }
