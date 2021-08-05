81ff25133b50d59b9fc927f4f663e60f34a156b6
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-08-05 20:07:26 +0200

    ref(Frontend): Route for loggedin user.

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,13 @@ const router = new VueRouter({
 		{ path: '/users', name: 'users', component: { template: '<users></users>' }},
 		{ path: '/restaurants', name: 'restautants', component: { template: '<restaurants></restaurants>' }},
 		{ path: '/profile/:id', name: 'profile', component: { template: '<profile></profile>' }},
+		{ path: '/profile', name: 'profile', component: { template: '<profile></profile>' }},
 		{ path: '/restaurants/:id/settings', name: 'restaurants', component: { template: '<restaurant-settings></restaurant-settings>' }},
 		{ path: '/restaurants/:id/orders', name: 'restaurants-orders', component: { template: '<restaurant-orders></restaurant-orders>' }},
 		{ path: '/restaurants/:id/buyers', name: 'restaurants-buyers', component: { template: '<restaurant-buyers></restaurant-buyers>' }},
 		{ path: '/restaurants/:id/manager', name: 'registration-manager', component: { template: '<registration-manager></registration-manager>' }},
 		{ path: '/restaurants/:id', name: 'restaurant', component: { template: '<restaurant></restaurant>' }},
+		
 		{ path: '/cart', name: 'cart', component: { template: '<cart></cart>' }},
 	
 	  ]
