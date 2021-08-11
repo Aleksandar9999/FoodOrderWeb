4aaefe415f3f1012e020af443252f1e16dbafb06
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-08-11 13:13:24 +0200

    ref(Backend): Only administrator can create access page for restaurant creation.

--- src/controller/RestaurantController.java
+++ src/controller/RestaurantController.java
@@ -1,56 +1,57 @@
 package controller;
+
 import com.google.gson.Gson;
 
 import beans.Manager;
 import beans.Restaurant;
+import enumerations.UserRole;
+import exceptions.UnauthorizedUserException;
 import service.RestaurantService;
-import service.UsersService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+
 public class RestaurantController {
 
-	private static RestaurantService restaurantService=new RestaurantService();
-	private static Gson gson=new Gson();
-	
-	public static Route handleGetRestaurantById = (Request request, Response response) -> {
+    private static RestaurantService restaurantService = new RestaurantService();
+    private static Gson gson = new Gson();
+
+    public static Route handleGetRestaurantById = (Request request, Response response) -> {
         response.type("application/json");
         String id = request.params("id");
-        if(id.equals("-1")) {
-            Restaurant rs=new Restaurant();
-            System.out.println(rs);
-            return gson.toJson(new Restaurant());
-        }
-        Restaurant restaurant=restaurantService.getById(id);
-        System.out.println(restaurant.getName());
+        Restaurant restaurant = restaurantService.getById(id);
         return gson.toJson(restaurant);
+
     };
     public static Route handleGetRestaurantByIdSettings = (Request request, Response response) -> {
         response.type("application/json");
         String id = request.params("id");
-        UsersService usersService=new UsersService();
-        Manager manager=(Manager) usersService.getByUsername(UserController.getLoggedingUsername(request));
-        if(manager.getRestaurant().getId().equals(id)){
-            Restaurant restaurant=restaurantService.getById(id);
-            return gson.toJson(restaurant);
-        }else{
+        try {
+            if (id.equals("-1")) {
+                validateAdministrator(request);
+                return gson.toJson(new Restaurant());
+            } else {
+                validateManager(request, id);
+                Restaurant restaurant = restaurantService.getById(id);
+                return gson.toJson(restaurant);
+            }
+        } catch (UnauthorizedUserException er) {
             response.status(401);
-            return ("Unauthorized");
-        } 
-        
+            return (er.getMessage());
+        }
     };
     public static Route handleAddNewRestaurant = (Request request, Response response) -> {
         response.type("application/json");
-        String body=request.body();
-        Restaurant restaurant=gson.fromJson(body, Restaurant.class);
+        String body = request.body();
+        Restaurant restaurant = gson.fromJson(body, Restaurant.class);
         restaurantService.addNew(restaurant);
         return gson.toJson(restaurant);
     };
 
     public static Route handleUpdateRestaurant = (Request request, Response response) -> {
         response.type("application/json");
-        String body=request.body();
-        Restaurant restaurant=gson.fromJson(body, Restaurant.class);
+        String body = request.body();
+        Restaurant restaurant = gson.fromJson(body, Restaurant.class);
         restaurantService.update(restaurant);
         return gson.toJson(restaurant);
     };
@@ -59,4 +60,15 @@ public class RestaurantController {
         response.type("application/json");
         return gson.toJson(restaurantService.getAllRestaurantsSorted());
     };
+
+    private static void validateAdministrator(Request request) {
+        if (!UserController.getLoggedingUser(request).getUserRole().equals(UserRole.Administrator))
+            throw new UnauthorizedUserException("Loggedin user is not administrator.");
     }
+
+    private static void validateManager(Request request, String restaurantId) {
+        if (!((Manager) UserController.getLoggedingUser(request)).getRestaurant().getId().equals(restaurantId))
+            throw new UnauthorizedUserException("Loggedin user is not manager of restaurant.");
+    }
+
+}
--- src/exceptions/UnauthorizedUserException.java
+++ src/exceptions/UnauthorizedUserException.java
@@ -4,5 +4,8 @@ public class UnauthorizedUserException extends RuntimeException {
     public UnauthorizedUserException() {
         super("Unauthorized user.");
     }
+    public UnauthorizedUserException(String message) {
+        super(message);
+    }
     
 }
