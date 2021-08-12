8f7b2e9574260023791179286efcf274865116a7
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-08-12 14:43:43 +0200

    feat(Backend): Calculation of restaurants avg rate.

--- src/repository/comment/CommentRepository.java
+++ src/repository/comment/CommentRepository.java
@@ -12,7 +12,7 @@ import generic.GenericFileRepository;
 import repository.restaurants.RestaurantRepository;
 import repository.users.UsersRepository;
 
-public class CommentRepository extends GenericFileRepository<Comment>{
+public class CommentRepository extends GenericFileRepository<Comment> {
 
     public CommentRepository() {
         super("./repo/comments.json");
@@ -21,27 +21,49 @@ public class CommentRepository extends GenericFileRepository<Comment>{
     @Override
     public HashMap<String, Comment> readAll() {
         String json = readFromFile();
-		Type type = new TypeToken<HashMap<String, Comment>>() {
-		}.getType();
-		HashMap<String, Comment> data = gson.fromJson(json, type);
-		if(data==null) data=new HashMap<>();
-		return margeWithObjects(data);
+        Type type = new TypeToken<HashMap<String, Comment>>() {
+        }.getType();
+        HashMap<String, Comment> data = gson.fromJson(json, type);
+        if (data == null)
+            data = new HashMap<>();
+        return margeWithObjects(data);
     }
 
-    
     private HashMap<String, Comment> margeWithObjects(HashMap<String, Comment> data) {
-        UsersRepository repository=new UsersRepository();
-        RestaurantRepository restaurantRepository=new RestaurantRepository();
+        UsersRepository repository = new UsersRepository();
+        RestaurantRepository restaurantRepository = new RestaurantRepository();
         for (Comment comment : data.values()) {
-            comment.setBuyer((Buyer)repository.getByUsername(comment.getBuyerId()));
+            comment.setBuyer((Buyer) repository.getByUsername(comment.getBuyerId()));
             comment.setRestaurant(restaurantRepository.getById(comment.getRestaurantId()));
         }
         return data;
     }
 
-    public List<Comment> getApprovedCommentsForRestaurant(String id){
-        List<Comment> list=getAll();
-        list.removeIf(com-> !com.isApproved() || !com.getRestaurant().getId().equals(id));
+    public List<Comment> getApprovedCommentsForRestaurant(String id) {
+        List<Comment> list = getAll();
+        list.removeIf(com -> !com.isApproved() || !com.getRestaurant().getId().equals(id));
         return list;
+    }
+
+    private HashMap<String, Comment> readAllWithoutMerge() {
+        String json = readFromFile();
+        Type type = new TypeToken<HashMap<String, Comment>>() {
+        }.getType();
+        HashMap<String, Comment> data = gson.fromJson(json, type);
+        if (data == null)
+            data = new HashMap<>();
+        return data;
+    }
+
+    public double getAvgRateByRestaurantId(String id) {
+        double rate = 0;
+        int i = 0;
+        for (Comment comment : readAllWithoutMerge().values()) {
+            if (comment.getRestaurantId().equals(id)) {
+                rate += comment.getMark();
+                i++;
+            }
+        }
+        return i != 0 ? rate / i : 0;
     }
 }
--- src/repository/restaurants/RestaurantRepository.java
+++ src/repository/restaurants/RestaurantRepository.java
@@ -1,18 +1,15 @@
 package repository.restaurants;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 import com.google.gson.reflect.TypeToken;
 
-import beans.Article;
 import beans.Restaurant;
 import generic.GenericFileRepository;
-import repository.articles.ArticlesRepository;
+import repository.comment.CommentRepository;
 
 public class RestaurantRepository extends GenericFileRepository<Restaurant> {
 	public RestaurantRepository() {
@@ -25,9 +22,15 @@ public class RestaurantRepository extends GenericFileRepository<Restaurant> {
 		Type type = new TypeToken<HashMap<String, Restaurant>>() {
 		}.getType();
 		HashMap<String, Restaurant> data = gson.fromJson(json, type);
+		return calculateAvgRate(data);
+	}
+	private HashMap<String, Restaurant> calculateAvgRate(HashMap<String, Restaurant> data){
+		CommentRepository repository=new CommentRepository();
+		for(Restaurant restaurant : data.values()){
+			restaurant.setAvgRate(repository.getAvgRateByRestaurantId(restaurant.getId()));
+		}
 		return data;
 	}
-
 	/*private HashMap<String, Restaurant> mergeWithArticles(HashMap<String, Restaurant> data) {
 		ArticlesRepository repository=new ArticlesRepository();
 		Collection<Restaurant> restaurants=data.values();
@@ -49,9 +52,4 @@ public class RestaurantRepository extends GenericFileRepository<Restaurant> {
 		});
 		return restaurants;
 	}
-
-	public List<Restaurant> getAllByAvgRate(String name) {
-		throw new UnsupportedOperationException("Implementirj");
-	}
-
 }
