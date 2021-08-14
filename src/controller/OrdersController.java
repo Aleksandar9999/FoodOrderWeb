d8122335f1be2ecb3c5105a9e362deb4a70f943a
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-08-14 10:51:03 +0200

    feat(Backend): Get orders for loggedin buyer.

--- src/controller/OrdersController.java
+++ src/controller/OrdersController.java
@@ -6,6 +6,8 @@ import com.google.gson.Gson;
 
 import beans.*;
 import exceptions.UnauthorizedUserException;
+import enumerations.UserRole;
+import exceptions.AccessException;
 import service.OrderService;
 import spark.Request;
 import spark.Response;
@@ -19,7 +21,7 @@ public class OrdersController {
         response.type("application/json");
         String id = request.params("id");
         try {
-            validateUser(request, id);
+            UserController.validateLoggedinManager(request, id);
             List<Order> orders = ordersService.getAllByRestaurant(id);
             return gson.toJson(orders);
         } catch (UnauthorizedUserException er) {
@@ -29,24 +31,20 @@ public class OrdersController {
 
     };
 
-    private static void validateUser(Request request, String restaurantId) {
-        Manager manager = (Manager) UserController.getLoggedingUser(request);
-        if (!manager.getRestaurant().getId().equals(restaurantId))
-            throw new UnauthorizedUserException();
-    }
-
     public static Route handleGetBuyersByRestaurant = (Request request, Response response) -> {
         response.type("application/json");
         String id = request.params("id");
         try {
-            validateUser(request, id);
+            UserController.validateLoggedinManager(request, id);
             List<User> orders = ordersService.getAllBuyersByRestaruantId(id);
             return gson.toJson(orders);
         } catch (UnauthorizedUserException er) {
             response.status(401);
             return (er.getMessage());
+        } catch (AccessException ex) {
+            response.status(403);
+            return (ex.getMessage());
         }
-
     };
 
     public static Route handleCreateOrder = (Request request, Response response) -> {
@@ -54,6 +52,45 @@ public class OrdersController {
         Cart cart = gson.fromJson(request.body(), Cart.class);
         ordersService.addNew(new Order(cart));
         return ("OK");
+    };
+
+    public static Route handleGetOrdersByBuyer = (Request request, Response response) -> {
+        response.type("application/json");
+        String username = request.params("id");
+        try {
+            if (username.equals("me")) {
+                User user = UserController.getLoggedingUser(request);
+                switch (user.getUserRole()) {
+                    case Buyer: {
+                        UserController.validateLoggedinBuyer(request);
+                        return gson.toJson(
+                                ordersService.getAllByBuyerUsername(UserController.getLoggedingUsername(request)));
+                    }
+                    case Deliverer:{
+
+                    }
+                    default:
+                        break;
+                }
+
+            }
+            return gson.toJson(ordersService.getAllByBuyerUsername(username));
+        } catch (RuntimeException e) {
+            response.status(401);
+            return e.getMessage();
+        }
+    };
+    public static Route handleUpdateOrder = (Request request, Response response) -> {
+        response.type("application/json");
+        Order order = gson.fromJson(request.body(), Order.class);
+        try {
+            UserController.validateLoggedinManager(request, order.getRestaurantId());
+            ordersService.update(order); 
+            return ("OK");
+        } catch (RuntimeException e) {
+            response.status(401);
+            return (e.getMessage());
+        }
     };
 
 }
