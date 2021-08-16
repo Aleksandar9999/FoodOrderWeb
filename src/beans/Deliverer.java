461863b4bd6b8506832d4c0a6f5e62bb8bcfcc69
Aleksandar9999 <aleksandar1999@uns.ac.rs>
2021-08-16 13:08:18 +0200

    feat(Backend): Deliverer order logic.

--- src/beans/Deliverer.java
+++ src/beans/Deliverer.java
@@ -1,7 +1,20 @@
 package beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deliverer extends User {
 	private List<Order> orders;
+	public void addOrder(Order order){
+		if(this.orders== null)
+			this.orders=new ArrayList<>();
+		this.orders.add(order);
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
 }
