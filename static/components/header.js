081c40358562778d186a333fed9885e1e9aae194
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-09-11 00:56:38 +0200

    feat[Frontend]: Custom header for all type of users

--- static/components/header.js
+++ static/components/header.js
@@ -1,22 +1,34 @@
-Vue.component("login", {
+Vue.component("custom-header", {
 	data: function () {
-		    return {
-		      id : -1,
-		      user: {username: '', password:''}
-		    }
+		return {
+			userRole: '',
+			restaurantId:'',
+		}
 	},
 	template: ` 
     <div id="header">
-    <h2 style="display: inline;">FoodOrder</h2>
-    <input style="display: inline; margin-left: 10%;" type="button" value="PROFILE">
-    </div>
+			<ul>
+				<li v-if="userRole == 'Administrator'"><a href="/index.html#/users">Korisnici</a></li>
+				<li v-if="userRole == 'Administrator'"><a href="/index.html#/users/suspicious">Sumnjivi korisnici</a></li>
+				<li v-if="userRole == 'Administrator'"><a href="/index.html#/registration/managers">Registruj menadzera</a></li>
+				<li v-if="userRole == 'Administrator'"><a href="/index.html#/registration/deliverers">Registruj dostavljaca</a></li>
+				<li><a href="/index.html#/restaurants">Restorani</a></li>
+				<li v-if="!userRole "><a href="/index.html">Prijavite se</a></li>
+				<li v-if="userRole"><a href="/index.html#/profile">Profil</a></li>
+				<li v-if="userRole == 'Buyer'"><a href="/index.html#/cart">Korpa</a></li>
+				<li v-if="userRole == 'Buyer' || userRole == 'Deliverer'"><a href="/index.html#/orders">Narudzbe</a></li>
+				<li v-if="userRole == 'Manager'"><a :href="'/index.html#/restaurants/'+restaurantId+'/settings'">Moj restoran</a></li>
+				<li v-if="userRole == 'Manager'"><a :href="'/index.html#/restaurants/'+restaurantId+'/orders'">Narudzbe</a></li>
+				<li v-if="userRole == 'Manager'"><a :href="'/index.html#/restaurants/'+restaurantId+'/buyers'">Kupci</a></li>
+				
+			</ul>
+	</div>
 `
-	, 
-	methods : {
-		loginUser : function () {
-			event.preventDefault();
-			axios.post('/rest/user/login', this.user).
-			then(response => (router.push(`/profile`)));
-		}
-	}
+	,
+	mounted() {
+		axios.get('/rest/users/me').then(response => {
+			this.userRole = response.data?.userRole;
+			this.restaurantId=response.data?.restaurantId
+		});
+	},
 });
